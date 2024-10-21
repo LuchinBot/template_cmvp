0b1583e9fcd588c959bff25651eaedf5094a0136
LuchinBot <luchinbot.2018@gmail.com>
2024-10-21 16:18:23 -0500

    Changes

---
+++ proce.php
@@ -0,0 +1,53 @@
+<?php include 'header.php'; ?>
+<section class="section-about container-fluid" style="background-image: url('resources/img/example.jpg');">
+	<div class="container">
+		<div class="wrapper">
+			<h1>Trámite <small style="font-weight: 200;">Nueva Colegiatura</small></h1>
+			<span>
+				Volver <i class="fa fa-angle-right mx-1"></i>
+				<a href="<?= $url; ?>">Trámites</a>
+			</span>
+		</div>
+	</div>
+</section>
+<section class="section-process-select">
+	<div class="container">
+		<div class="wrapper">
+			<h2 class="title">REQUISITOS</h2>
+			<div class="context">
+				<ul>
+					<li>Ficha de Inscripción (Incluir en la ficha la Modalidad de Titulación, pedir al solicitante a voluntad el envío de una copia de la tesis en formato PDF para su almacenamiento y publicación en la página web del CMVP).</li>
+					<li>Carta Declaratoria del F.A.F.</li>
+					<li>Copia del Título Profesional autenticada por el CMVP.</li>
+					<li>Recibo de pago de la cuota de Inscripción.</li>
+					<li>Recibo de pago de la cuota de Registro.</li>
+					<li>Copia del Documento de identidad vigente</li>
+				</ul>
+			</div>
+			<h2 class="title">DERECHOS DE PAGO</h2>
+			<div class="context">
+				<p>CUOTA DE INSCRIPCIÓN 25 % UIT MÁS S/. 100.00 CUOTA DE REGISTRO</p>
+			</div>
+			<h2 class="title">evaluación</h2>
+			<div class="context">
+				<p>7 – 30 días</p>
+			</div>
+			<h2 class="title">Dependencia del Procedimiento</h2>
+			<div class="context">
+				<ul>
+					<li>Mesa de partes del Colegio Médico veterinario Departamental (CMVD), según corresponda.</li>
+					<li>El Consejo del CMVD lo revisara y enviara al Colegio Médico Veterinario del Perú (CMVP).</li>
+				</ul>
+			</div>
+			<h2 class="title">Autoridad que aprueba el procedimiento </h2>
+			<div class="context">
+				<p>Consejo Nacional del CMVP</p>
+			</div>
+			<h2 class="title">Autoridad que resuelve el recurso de apelación<h2>
+					<div class="context">
+						<p>Decano del CMVP</p>
+					</div>
+		</div>
+	</div>
+</section>
+<?php include 'footer.php'; ?>
