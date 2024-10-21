5428ad246112124ecbf48e33e049316b573c1f21
Alden <teambemaster@gmail.com>
2024-10-21 18:14:45 -0500

    Charge Course

--- courses.php
+++ courses.php
@@ -15,18 +15,21 @@
 		<div class="col p-0 d-flex">
 			<div class="col-md-6 p-0">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/example.jpg"
-						style="background-image: url('resources/img/example.jpg');">
+					<div class="img-course" data-url="resources/img/curso1.jpg"
+						style="background-image: url('resources/img/curso1.jpg');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 202
+						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 2023
 						</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vel
+						<p>Dirigido a profesionales y público general interesados en mejorar sus conocimientos sobre el manejo integral de cuyes. El curso aborda temas claves como la implementación de medidas de bioseguridad para prevenir enfermedades, técnicas reproductivas avanzadas, prácticas de sanidad para garantizar el bienestar animal y manejo reproductivo de razas generadas en el INIA.
 						<ul>
-							<li>Ponente: Ingeniero Luchi</li>
-							<li>Lorem ipsum dolor sit amet</li>
-							<li>Lorem ipsum dolor sit amet</li>
+							<li><b> Ponente 1: </b> Ing. Lilia Chauca Francia</li>
+							<li><b> Ponente 2: </b> M.V. Meylil Huamán Alcántara</li>
+							<li><b>Costo:</b> Colegiados S/ 100.00 - Público S/ 120.00</li>
+							<li><b>Fecha de Inicio:</b> Sábado 20 de Mayo</li>
+							<li><b>Lugar:</b> Auditorio de la Municipalidad Provincial de Moyobamba</li>
+
 						</ul>
 						</p>
 					</div>
@@ -34,20 +37,22 @@
 			</div>
 			<div class="col-md-6 p-0">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/slider1.jpg"
-						style="background-image: url('resources/img/slider1.jpg');">
+					<div class="img-course" data-url="resources/img/curso2.jpg"
+						style="background-image: url('resources/img/curso2.jpg');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 202
+						<h3>Introducción a la Veterinaria Forense 2024
 						</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vel
-							repudiandae consectetur fuga. Consequuntur ut, animi eveniet quod provident quisquam
+						<p>Dirigido a médicos veterinarios, bachilleres y estudiantes avanzados en veterinaria. Ofrece una visión integral sobre el papel del perito médico veterinario en investigaciones forenses. Los temas incluyen criminología y criminalística animal, agresión sexual animal, y manejo de conflictos relacionados con animales.
 
 						<ul>
-							<li>Ponente: Ingeniero Luchi</li>
-							<li>Lorem ipsum dolor sit amet</li>
-							<li>Lorem ipsum dolor sit amet</li>
+							<li><b> Ponente 1: </b> M.V. Jhon P. Islachin .H</li>
+							<li><b>Costo:</b> Preventa S/ 200.00 - Después S/ 250.00</li>
+							<li><b>Fecha de Inicio:</b> 01 de Junio</li>
+							<li><b>Fecha de Cierre:</b> 02 de Junio</li>
+							<li><b>Lugar:</b> Auditorio de la Municipalidad Provincial de San Martín</li>
+
 						</ul>
 						</p>
 					</div>
