0ae2d642a4cb7f2e780f03ba60e2fec9be4584a6
Alden <teambemaster@gmail.com>
2024-10-22 10:40:43 -0500

    Charge course

--- courses.php
+++ courses.php
@@ -13,22 +13,23 @@
 <section class="section-courses container-fluid">
 	<div class="container p-0">
 		<div class="col p-0 d-flex">
-			<div class="col-md-6">
+		<div class="col-md-6">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/curso1.jpg"
-						style="background-image: url('resources/img/curso1.jpg');">
+					<div class="img-course" data-url="resources/img/curso3.jpg"
+						style="background-image: url('resources/img/curso3.jpg');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 2023
+						<h3>Anestesia Veterinaria (2024)
 						</h3>
-						<p>Dirigido a profesionales y público general interesados en mejorar sus conocimientos sobre el manejo integral de cuyes. El curso aborda temas claves como la implementación de medidas de bioseguridad para prevenir enfermedades, técnicas reproductivas avanzadas, prácticas de sanidad para garantizar el bienestar animal y manejo reproductivo de razas generadas en el INIA.
+						<p>Dirigido a médicos veterinarios, estudiantes y afines en el área. Impartido por el M.V. Darwin Llaja Llontop, se enfoca en los principios generales de la anestesia, la evaluación y preparación del paciente, así como el uso de fármacos anestésicos. Los participantes aprenderán sobre monitorización, técnicas transquirúrgicas, infusión continua (TIVA) e intubación endotraqueal.
+
 						<ul>
-							<li><b> Ponente 1: </b> Ing. Lilia Chauca Francia</li>
-							<li><b> Ponente 2: </b> M.V. Meylil Huamán Alcántara</li>
-							<li><b>Costo:</b> Colegiados S/ 100.00 - Público S/ 120.00</li>
-							<li><b>Fecha de Inicio:</b> Sábado 20 de Mayo</li>
-							<li><b>Lugar:</b> Auditorio de la Municipalidad Provincial de Moyobamba</li>
+							<li><b> Ponente: </b> Mv. Darwin Llaja Llontop</li>
+							<li><b>Costo:</b> Preventa S/ 200.00 - Después S/ 250.00</li>
+							<li><b>Fecha de Inicio:</b> 03 de febrero</li>
+							<li><b>Fecha de Cierre:</b> 04 de febrero</li>
+							<li><b>Lugar:</b> Escuela de Medicina Veterinaria - Banda de Shilcayo</li>
 
 						</ul>
 						</p>
@@ -42,16 +43,62 @@
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Introducción a la Veterinaria Forense 2024
+						<h3>Introducción a la Veterinaria Forense (2024)
 						</h3>
 						<p>Dirigido a médicos veterinarios, bachilleres y estudiantes avanzados en veterinaria. Ofrece una visión integral sobre el papel del perito médico veterinario en investigaciones forenses. Los temas incluyen criminología y criminalística animal, agresión sexual animal, y manejo de conflictos relacionados con animales.
 
 						<ul>
-							<li><b> Ponente 1: </b> M.V. Jhon P. Islachin .H</li>
+							<li><b> Ponente: </b> Mv. Jhon P. Islachin .H</li>
 							<li><b>Costo:</b> Preventa S/ 200.00 - Después S/ 250.00</li>
 							<li><b>Fecha de Inicio:</b> 01 de Junio</li>
 							<li><b>Fecha de Cierre:</b> 02 de Junio</li>
 							<li><b>Lugar:</b> Auditorio de la Municipalidad Provincial de San Martín</li>
+
+						</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="box">
+					<div class="img-course" data-url="resources/img/curso4.png"
+						style="background-image: url('resources/img/curso4.png');">
+						<i class="fa far fa-eye"></i>
+					</div>
+					<div class="text">
+						<h3>Curso reproducción e inseminación artificial en bovinos (2023)
+						</h3>
+						<p>Orientado a profesionales y estudiantes interesados en el manejo reproductivo del ganado bovino. Impartido por el Ing. Zootecnista Oscar Rengifo Garrido, aborda temas como anatomía y fisiología reproductiva, ultrasonografía, protocolos de sincronización, e inseminación artificial (IATF).
+
+						<ul>
+							<li><b> Ponente 1: </b> Ing. Orcar Rengifo Garrifo</li>
+							<li><b> Ponente 2: </b> Ing. Zootecnista (UNALM)</li>
+							<li><b>Costo:</b> Profesionales S/ 350.00 - Estudiantes S/ 150.00</li>
+							<li><b>Fecha de Inicio:</b> 30 de Septiembre</li>
+							<li><b>Fecha de Cierre:</b> 01 de Octubre</li>
+							<li><b>Lugar:</b> Escuela de Medicina Veterinaria - Banda de Shilcayo</li>
+
+						</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="box">
+					<div class="img-course" data-url="resources/img/curso1.jpg"
+						style="background-image: url('resources/img/curso1.jpg');">
+						<i class="fa far fa-eye"></i>
+					</div>
+					<div class="text">
+						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes (2023)
+						</h3>
+						<p>Dirigido a profesionales y público general interesados en mejorar sus conocimientos sobre el manejo integral de cuyes. El curso aborda temas claves como la implementación de medidas de bioseguridad para prevenir enfermedades, técnicas reproductivas avanzadas, prácticas de sanidad para garantizar el bienestar animal y manejo reproductivo de razas generadas en el INIA.
+						<ul>
+							<li><b> Ponente 1: </b> Ing. Lilia Chauca Francia</li>
+							<li><b> Ponente 2: </b> Mv. Meylil Huamán Alcántara</li>
+							<li><b>Costo:</b> Colegiados S/ 100.00 - Público S/ 120.00</li>
+							<li><b>Fecha de Inicio:</b> Sábado 20 de Mayo</li>
+							<li><b>Lugar:</b> Auditorio de la Municipalidad Provincial de Moyobamba</li>
 
 						</ul>
 						</p>
