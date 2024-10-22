c0e200a44f7ddf6d4dea6e8871be75fadf49936d
Alden <teambemaster@gmail.com>
2024-10-22 11:34:46 -0500

    Charge event

--- events.php
+++ events.php
@@ -15,6 +15,75 @@
 		<div class="col p-0 d-flex">
 			<div class="col-md-6">
 				<div class="box">
+					<div class="img-course" data-url="resources/img/evento6.png"
+						style="background-image: url('resources/img/evento6.png');">
+						<i class="fa far fa-eye"></i>
+					</div>
+					<div class="text">
+						<h3>Conferencia Análisis Situacional de la Buiatría en San Martín (2024)
+						</h3>
+						<p>El Colegio Médico Veterinario Departamental de San Martín organiza una conferencia sobre el Análisis situacional de la Buiatría en San Martín y la conformación de la Junta Directiva de la Sociedad de Buiatría del Perú en la región. La conferencia será presentada por el Dr. Raúl Santibañez Vivanco, presidente de la Sociedad Peruana de Buiatría.
+
+						<ul>
+							<li><b>Ponente: </b> Dr. Raúl Santibábez Vivanco</li>
+							<li><b>Fecha de Inicio:</b> 10 de Julio</li>
+							<li><b>Hora:</b> 7:00 pm</li>
+							<li><b>Modalidad:</b> Presencial</li>
+							<li><b>Lugar:</b> Colegio Médico Veterinario de San Martín</li>
+						</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="box">
+					<div class="img-course" data-url="resources/img/evento5.png"
+						style="background-image: url('resources/img/evento5.png');">
+						<i class="fa far fa-eye"></i>
+					</div>
+					<div class="text">
+						<h3>Charla Informativa: Requisitos para la Emisión del Certificado Sanitario de Exportación de Mascotas, Registro y Fiscalización de Insumos Pecuarios (2024)
+						</h3>
+						<p>Esta Charla Informativa, organizada por SENASA Perú, tratará sobre los requisitos para
+							la emisión del certificado sanitario de exportación de mascotas, así como el registro
+							y fiscalización de insumos pecuarios.
+
+						<ul>
+							<li><b>Fecha de Inicio:</b> 19 de Abril</li>
+							<li><b>Hora:</b> 9:00 am</li>
+							<li><b>Modalidad:</b> Presencial</li>
+							<li><b>Lugar:</b> Auditorio del SENASA - San Martín</li>
+						</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="box">
+					<div class="img-course" data-url="resources/img/evento3.png"
+						style="background-image: url('resources/img/evento3.png');">
+						<i class="fa far fa-eye"></i>
+					</div>
+					<div class="text">
+						<h3>Charla Magistral: Problemas Rerpoductivos y Respiratorios en Bovinos - Un Paso Adelante en el Control de Ectoparásitos (2024)
+						</h3>
+						<p>El evento se centra en los problemas reproductivos y respiratorios en bovinos,
+							así como en el control avanzado de ectoparásitos. Los expertos veterinarios
+							ofrecerán una visión profunda sobre estas áreas críticas en la salud animal.
+						<ul>
+							<li><b>Ponente 1: </b> Mv. Jorge del Campo Santa Cruz</li>
+							<li><b>Ponente 2: </b> Mv.M.Cs. Genaro Palomino Lozano</li>
+							<li><b>Fecha de Inicio:</b> 22 de Mayo</li>
+							<li><b>Hora:</b> 2:00 pm</li>
+							<li><b>Modalidad:</b> Presencial</li>
+							<li><b>Lugar:</b> Auditorio del Senasa</li>
+						</ul>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="box">
 					<div class="img-course" data-url="resources/img/evento1.png"
 						style="background-image: url('resources/img/evento1.png');">
 						<i class="fa far fa-eye"></i>
@@ -27,6 +96,7 @@
 							<li><b> Ponente: </b> Mv. Manuel Albetis Apolaya</li>
 							<li><b>Fecha de Inicio:</b> Viernes 11 Junio</li>
 							<li><b>Hora:</b> 7:00 pm</li>
+							<li><b>Modalidad:</b> Virtual</li>
 							<li><b>Link:</b> https://www.google.com/</li>
 						</ul>
 						</p>
@@ -51,6 +121,7 @@
 							<li><b>Ponente 2: </b> Mv.M.Cs. Genaro Palomino Lozano</li>
 							<li><b>Fecha de Inicio:</b> 25 de Mayo</li>
 							<li><b>Hora:</b> 6:00 pm</li>
+							<li><b>Modalidad:</b> Presencial</li>
 							<li><b>Lugar:</b> Auditorio de Videoconferencia de la Universidad Nacional de San Martín</li>
 						</ul>
 						</p>
@@ -59,22 +130,25 @@
 			</div>
 			<div class="col-md-6">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/evento3.png"
-						style="background-image: url('resources/img/evento3.png');">
+					<div class="img-course" data-url="resources/img/evento4.png"
+						style="background-image: url('resources/img/evento4.png');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Charla Magistral: Problemas Rerpoductivos y Respiratorios en Bovinos - Un Paso Adelante en el Control de Ectoparásitos (2024)
+						<h3>Charla Virtual: Función de los Médicos Veterinarios en los Mataderos (2023)
 						</h3>
-						<p>El evento se centra en los problemas reproductivos y respiratorios en bovinos,
-							así como en el control avanzado de ectoparásitos. Los expertos veterinarios
-							ofrecerán una visión profunda sobre estas áreas críticas en la salud animal.
+						<p>Este programa incluye dos sesiones dirigidas a profesionales veterinarios.
+							El 6 de julio, el M.V. Percy Lezama Vigo discutirá el rol de los médicos
+							veterinarios en los mataderos, abordando su importancia en la seguridad
+							alimentaria.
+
 						<ul>
-							<li><b>Ponente 1: </b> Mv. Jorge del Campo Santa Cruz</li>
-							<li><b>Ponente 2: </b> Mv.M.Cs. Genaro Palomino Lozano</li>
-							<li><b>Fecha de Inicio:</b> 22 de Mayo</li>
-							<li><b>Hora:</b> 2:00 pm</li>
-							<li><b>Lugar:</b> Auditorio del Senasa</li>
+							<li><b>Ponente 1: </b> Mv. Percy Lezama Vigo</li>
+							<li><b>Ponente 2: </b> Mv. Manuel Albetis Apolaya</li>
+							<li><b>Fecha de Inicio:</b> 06 y 07 de Julio</li>
+							<li><b>Hora:</b> 8:00 pm</li>
+							<li><b>Modalidad:</b> Virtual</li>
+							<li><b>Lugar:</b> https://meet.google.com/cgz-nhgt-nvh</li>
 						</ul>
 						</p>
 					</div>
