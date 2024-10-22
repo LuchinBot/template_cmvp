ff3af25fe5e5c44a02ddab0f73a14be5ea7311fa
Alden <teambemaster@gmail.com>
2024-10-21 22:09:09 -0500

    Charge Event

--- events.php
+++ events.php
@@ -15,20 +15,19 @@
 		<div class="col p-0 d-flex">
 			<div class="col-md-6">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/example.jpg"
-						style="background-image: url('resources/img/example.jpg');">
+					<div class="img-course" data-url="resources/img/evento1.png"
+						style="background-image: url('resources/img/evento1.png');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 202
+						<h3>Charla Virtual: Complejo respiratorio en cerdos y su inmunización (2023)
 						</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vel
-							repudiandae consectetur fuga. Consequuntur ut, animi eveniet quod provident quisquam
-
+						<p>Este evento virtual trata sobre el complejo respiratorio en cerdos y las estrategias de inmunización para prevenir esta enfermedad. Presentado por el M.V. Manuel Albetis Apolaya, está dirigido a profesionales interesados en la salud porcina.
 						<ul>
-							<li>Ponente: Ingeniero Luchi</li>
-							<li>Lorem ipsum dolor sit amet</li>
-							<li>Lorem ipsum dolor sit amet</li>
+							<li><b> Ponente: </b> Mv. Manuel Albetis Apolaya</li>
+							<li><b>Fecha de Inicio:</b> Viernes 11 Junio</li>
+							<li><b>Hora:</b> 7:00 pm</li>
+							<li><b>Link:</b> https://www.google.com/</li>
 						</ul>
 						</p>
 					</div>
@@ -36,20 +35,23 @@
 			</div>
 			<div class="col-md-6">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/slider1.jpg"
-						style="background-image: url('resources/img/slider1.jpg');">
+					<div class="img-course" data-url="resources/img/evento2.jpg"
+						style="background-image: url('resources/img/evento2.jpg');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 202
+						<h3>Charla Magistral: Inmunización en Bovinos (2023)
 						</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vel
-							repudiandae consectetur fuga. Consequuntur ut, animi eveniet quod provident quisquam
+						<p>Este evento, dirigido por expertos veterinarios, aborda temas como la inmunización
+							en bovinos y la prevención de la rabia bovina. Presentado en Tarapoto, San Martín,
+							es una oportunidad para adquirir conocimientos sobre la salud animal.
 
 						<ul>
-							<li>Ponente: Ingeniero Luchi</li>
-							<li>Lorem ipsum dolor sit amet</li>
-							<li>Lorem ipsum dolor sit amet</li>
+							<li><b>Ponente 1: </b> Mv. Jorge del Campo Santa Cruz</li>
+							<li><b>Ponente 2: </b> Mv.M.Cs. Genaro Palomino Lozano</li>
+							<li><b>Fecha de Inicio:</b> 25 de Mayo</li>
+							<li><b>Hora:</b> 6:00 pm</li>
+							<li><b>Lugar:</b> Auditorio de Videoconferencia de la Universidad Nacional de San Martín</li>
 						</ul>
 						</p>
 					</div>
@@ -57,20 +59,22 @@
 			</div>
 			<div class="col-md-6">
 				<div class="box">
-					<div class="img-course" data-url="resources/img/slider1.jpg"
-						style="background-image: url('resources/img/slider1.jpg');">
+					<div class="img-course" data-url="resources/img/evento3.png"
+						style="background-image: url('resources/img/evento3.png');">
 						<i class="fa far fa-eye"></i>
 					</div>
 					<div class="text">
-						<h3>Curso de Bioseguridad Reproducción, Sanidad e Innovación en la crianza de Cuyes 202
+						<h3>Charla Magistral: Problemas Rerpoductivos y Respiratorios en Bovinos - Un Paso Adelante en el Control de Ectoparásitos (2024)
 						</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem vel
-							repudiandae consectetur fuga. Consequuntur ut, animi eveniet quod provident quisquam
-
+						<p>El evento se centra en los problemas reproductivos y respiratorios en bovinos,
+							así como en el control avanzado de ectoparásitos. Los expertos veterinarios
+							ofrecerán una visión profunda sobre estas áreas críticas en la salud animal.
 						<ul>
-							<li>Ponente: Ingeniero Luchi</li>
-							<li>Lorem ipsum dolor sit amet</li>
-							<li>Lorem ipsum dolor sit amet</li>
+							<li><b>Ponente 1: </b> Mv. Jorge del Campo Santa Cruz</li>
+							<li><b>Ponente 2: </b> Mv.M.Cs. Genaro Palomino Lozano</li>
+							<li><b>Fecha de Inicio:</b> 22 de Mayo</li>
+							<li><b>Hora:</b> 2:00 pm</li>
+							<li><b>Lugar:</b> Auditorio del Senasa</li>
 						</ul>
 						</p>
 					</div>
